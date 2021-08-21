46db763efda10fb2a8524d7acd6802f03d0623d5
HenricoW <henrico.ashley.White@gmail.com>
2021-08-21 23:19:59 +0200

    emission state enum, simplified user state. fns: added to stake & createEra

--- contracts/Staker.sol
+++ contracts/Staker.sol
@@ -23,9 +23,10 @@ contract Staker is Ownable {
     uint public endBlock;   // will be calculated
     uint public releaseAmount;
     IERC20 public LPtoken;
+    IERC20 public ETBtoken;
 
-    bool public isEmitting; // use to detect if new Emission Era can be set
-    // could replace with enum EmissionStatus { notStarted, Initialized, EraActive, EraEnded }
+    enum EmissionStates { NOT_STARTED, INITIALIZED, ERA_ACTIVE, ERA_ENDED }
+    EmissionStates emissionStatus = EmissionStates.ERA_ENDED;
 
     // STAKING STATUS:
     uint public lastCheckPt; // latest block where staked amount changed
@@ -37,32 +38,66 @@ contract Staker is Ownable {
     CheckPoint[] public checkPtRecord; // keeps track of evolution of checkpoint details
     uint public tokensClaimed; // total number of reward tokens claimed since staking genisis
 
-    // USER STATUS:
-    // keep track of each user's stake amount change, with block number
-    struct UsrChkPt { uint blocknum; uint stakeAmt; } // user stake amount track record
-    struct RewardStatus { uint calcultadAt; uint rewardTotal; } // latest user reward calculation -> OPTIONS: could calc at every new stake chg OR only at unstake
-    struct UserStatus { UsrChkPt[] userRecord; RewardStatus userRewStatus; }
-    mapping( address => UserStatus[] ) public stateOfUsers; // -> use this mapping to iterate over [checkPtRecord] to calc stake reward (and optionally update latest total stake reward)
+    // USER STATUS: keep track of each user's stake amount change, with block number
+    struct UsrChkPt { uint mainChkPtIndex; uint stakeAmt; } // user stake amount track record [mainChkPtIndex] -> array index of (staking status) CheckPoint[]
+    struct UserState { 
+        UsrChkPt[] userRecord;      // track record of all stake changes (stake & unstake)
+        uint idxOfLastClaim;        // main CheckPoint[] array index where latest total reward was calculated
+        uint latestTotalReward;     // total reward calculated at full or partial unstake (use fraction of this for partial unstake)
+        uint latestRemainingReward; // remainder of reward left if last claim was partial claim
+    }
+    mapping( address => UserState ) public stateOfUsers; // -> use this mapping to iterate over [checkPtRecord] to calc stake reward (and optionally update latest total stake reward)
+
+    // EVENTS:
+    event EmissionStarted(uint indexed startBlock, uint endBlock, uint releaseAmount);
 
     // -----------------------------
     //      MAIN CONTRACT BODY:
     // -----------------------------
-    constructor (address _ETBaddress) {
-        LPtoken = IERC20(_ETBaddress);
+    constructor (address _lpTokenETB, address _ETBtoken) {
+        LPtoken = IERC20(_lpTokenETB);
+        ETBtoken = IERC20(_ETBtoken);
     }
 
     // create a new Emission Era - only owner
     // owner must have enough tokens to fund _releaseAmount
-    // OPTION: could change to createEra(_durationInBlocks, _releaseAmount) AND a startEra() function !!
-    function createEra(uint _startBlock, uint _endBlock, uint _releaseAmount) external onlyOwner returns(bool) {
-        startBlock = _startBlock;
-        endBlock = _endBlock;
-        releaseAmount = _releaseAmount;
+    function createEra(uint _durationInBlocks, uint _releaseAmount) external onlyOwner {
+        require(emissionStatus == EmissionStates.ERA_ENDED, "Staker#createEra: Emission Era currently in progress");
+        require(_releaseAmount > 0, "Staker#createEra: Release amount cannot be zero");
+        require(_durationInBlocks > 0, "Staker#createEra: Block duration cannot be zero");
+        bool txSuccess = ETBtoken.transferFrom(msg.sender, address(this), _releaseAmount);
+        require(txSuccess, "Staker#createEra: ETB token transfer failed");
 
-        return LPtoken.transferFrom(msg.sender, address(this), _releaseAmount);
+        durationInBlocks = _durationInBlocks;
+        releaseAmount = _releaseAmount;
+        emissionStatus = EmissionStates.INITIALIZED;
     }
 
-    // user stake (deposit)
+    // Users stake (deposit) ETB LP tokens
+    function stake(uint _LPamount) external {
+        require(emissionStatus == EmissionStates.INITIALIZED || emissionStatus == EmissionStates.ERA_ACTIVE, "Staker#stake: Emission Era has not started");
+        require(_LPamount > 0, "Staker#stake: LP amount cannot be zero");
+        bool txSuccess = LPtoken.transferFrom(msg.sender, address(this), _LPamount);
+        require(txSuccess, "Staker#stake: ETB token transfer failed");
+
+        // 1st staker starts the emitting process (prevents [reward / 0] later in logic)
+        if(emissionStatus == EmissionStates.INITIALIZED) {
+            emissionStatus == EmissionStates.ERA_ACTIVE;
+            startBlock = block.number;
+            endBlock = startBlock + durationInBlocks;
+
+            emit EmissionStarted(startBlock, endBlock, releaseAmount);
+        }
+        
+        // staking state update
+        totalStaked += _LPamount;
+        CheckPoint memory chkPt = CheckPoint(block.number, totalStaked);
+        checkPtRecord.push(chkPt);
+
+        // user state update
+        UsrChkPt memory usrChkPt = UsrChkPt((checkPtRecord.length - 1), _LPamount);
+        stateOfUsers[msg.sender].userRecord.push(usrChkPt);
+    }
 
     // OPTIONAL? user claim (only rewards) -> @ least internal either way
 
