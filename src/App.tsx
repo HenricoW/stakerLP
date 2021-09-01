ce0b25a5cfa9dbeb52af9340afe53a20c7ef4828
HenricoW <henrico.ashley.White@gmail.com>
2021-09-01 17:55:56 +0200

    user panel & web3 connection

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,31 @@ import Navbar from "./components/Navbar";
 import Torus from "@toruslabs/torus-embed";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
+import UserPanel from "./components/UserPanel";
+
+// artifacts
+import MockETB_afct from "./artifacts/MockETB.json";
+import Staker_afct from "./artifacts/Staker.json";
+import LPetb_afct from "./artifacts/LPetb.json";
+import IERC20_afct from "./artifacts/IERC20.json";
+
+type artifact = {
+    [key: string]: any;
+    networks: {
+        [key: string]: any;
+    };
+};
+
+const Staker: artifact = Staker_afct;
+const MockETB: artifact = MockETB_afct;
+const LPetb: artifact = LPetb_afct;
+const IERC20: artifact = IERC20_afct;
+
+// testnet values
+const ETB_test = "";
+const LP_ETB_test = "";
+const ETB_main = "0x7ac64008fa000bfdc4494e0bfcc9f4eff3d51d2a"; // ETB on mainnet
+const LP_ETB_main = "0xdB44C35Cd6C378eB9e593d4c7243118064172fb2"; // PCS_ETB_WBNB on mainnet
 
 const providerOptions = {
     // authereum: { package: Autherum },
@@ -24,11 +49,43 @@ export const getWeb3 = async () => {
     return { _web3, _provider };
 };
 
+export const getContracts = async (web3: Web3) => {
+    const networkId = await web3.eth.net.getId();
+
+    const stakerAddr: string = Staker.networks[networkId.toString()].address;
+    const stakerContr = new web3.eth.Contract(Staker.abi, stakerAddr);
+    // const stakerContr = null;
+
+    let mETBcontr, lpTkncontr;
+    switch (networkId) {
+        case 5777: // ganache
+            const mETBaddr: string = MockETB.networks[networkId.toString()].address;
+            mETBcontr = new web3.eth.Contract(MockETB.abi, mETBaddr);
+            const lpTknAddr: string = LPetb.networks[networkId.toString()].address;
+            lpTkncontr = new web3.eth.Contract(LPetb.abi, lpTknAddr);
+            break;
+        case 53: // BSC testnet
+            mETBcontr = new web3.eth.Contract(IERC20.abi, ETB_test);
+            lpTkncontr = new web3.eth.Contract(IERC20.abi, LP_ETB_test);
+            break;
+        case 56: // BSC mainnet
+            mETBcontr = new web3.eth.Contract(IERC20.abi, ETB_main);
+            lpTkncontr = new web3.eth.Contract(IERC20.abi, LP_ETB_main);
+            break;
+        default:
+            mETBcontr = null;
+            lpTkncontr = null;
+    }
+
+    return [stakerContr, mETBcontr, lpTkncontr];
+};
+
 export const chainData = new Map<number, string>([
     [0, "Unsupported Network"],
-    // [1, "ETH Mainnet"],
+    [1, "ETH Mainnet"],
     [97, "BSC Testnet"],
     [56, "BSC Mainnet"],
+    [1337, "Local Dev"],
 ]);
 
 function App() {
@@ -38,6 +95,7 @@ function App() {
     const [provider, setProvider] = useState<any>(undefined);
 
     const getChainId = async () => (web3 ? await web3.eth.getChainId() : 0);
+    // const getW3Contr = async () => (web3 ? await getContracts(web3) : [null, null, null]);
 
     if (web3) {
         let addr = "";
@@ -60,32 +118,14 @@ function App() {
         provider.on("chainChanged", (chainId: number) => setChainId(chainId));
     }
 
-    // const disconnect = () => {
-    //     if (provider) {
-    //         setProvider(undefined);
-    //         setAddress("");
-    //         setChainId(0);
-    //         // setWeb3(undefined)
-    //     }
-    // };
-
     return (
         <>
-            <Navbar
-                chainId={chainId}
-                address={address}
-                setWeb3={setWeb3}
-                setProvider={setProvider}
-                // disconnect={disconnect}
-            />
+            <Navbar chainId={chainId} address={address} setWeb3={setWeb3} setProvider={setProvider} />
             <div className="main">
                 <div className="modal"></div>
                 <section className="container era-info"></section>
                 <section className="container admin-panel"></section>
-                <section className="container user-panel">
-                    <div className="card-slots"></div>
-                    <div className="card-staking"></div>
-                </section>
+                <UserPanel />
             </div>
             <Footer />
         </>
