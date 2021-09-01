222acc80fc3f6dd5517ce9121e67e2b96999cac3
HenricoW <henrico.ashley.White@gmail.com>
2021-09-01 17:52:47 +0200

    updates & additions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
+import "./Footer.css";
+
 function Footer() {
-    return <footer className="container">Copyright Blah Blah - 2021</footer>;
+    return <footer className="footer">Copyright Eat The Blocks - 2021</footer>;
 }
 
 export default Footer;
