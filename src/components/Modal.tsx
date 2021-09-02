9f1e1c13978e4e4cf9dd850c3a93e027a9b4fb81
HenricoW <henrico.ashley.White@gmail.com>
2021-09-02 18:46:12 +0200

    tx status to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,16 +2,27 @@ import "./Modal.css";
 
 type ModalProps = {
     modalTitle: string;
+    txStatus: string;
     resetModal: () => void;
     children: React.ReactNode;
 };
 
-function Modal({ modalTitle, resetModal, children }: ModalProps) {
+const txStatusMssgs: { [key: string]: string } = {
+    pending: "Please follow your wallet prompts",
+    success: "Transaction successful!",
+    failed: "Transaction failed",
+};
+
+function Modal({ modalTitle, txStatus, resetModal, children }: ModalProps) {
     return (
         <div className="modal-bg" onClick={resetModal}>
             <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <div className="modal-head">Stake your LP</div>
-                {children}
+                <div className="modal-head">{modalTitle}</div>
+                {txStatus === "not sent" ? (
+                    children
+                ) : (
+                    <div className="modal-body tx-message">{txStatusMssgs[txStatus]}</div>
+                )}
             </div>
         </div>
     );
